03804b8432a28a50e630260e79c9c1d4df94f18b
lucas <git.frantic221@passmail.com>
2026-09-02 15:21:27 -0300

    jolteon picture added for the repo readme

--- app/backend/infrastructure/database/schemas.py
+++ app/backend/infrastructure/database/schemas.py
@@ -10,17 +10,3 @@ tasks_table = Table(
     Column("status", Integer),
     Column("deadline", Date)
 )
-
-users_table = Table(
-    "users",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String)
-)
-
-products_table = Table(
-    "products",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String)
-)
